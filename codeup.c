fd04562cde09b06a6de12d601f11cc0691c6da26
STARTERGATE <32726809+startergate@users.noreply.github.com>
2018-08-20 08:55:22 +0900

    2018.8.17 Study

--- codeup.c
+++ codeup.c
@@ -1,1 +1,15 @@
-/*main(){puts("all");}*/
+#include <stdio.h>
+
+int n, m;
+
+int circle(int);
+
+int main()
+{
+	scanf("%d%d", &n, &m);
+	printf("%d\n", max(n, m));
+}
+
+int circle(int r) {
+	return 3.14 * r * r;
+}
